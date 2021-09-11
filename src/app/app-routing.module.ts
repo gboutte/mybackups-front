ff45d6ddd886740f31f575c7b57048a5687c9624
gboutte <btte.gregory@gmail.com>
2021-09-11 02:17:37 +0200

    Page 404 + titles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConfigurationComponent } from './Pages/ConfigurationComponent/configuration.component';
 import { DashboardComponent } from './Pages/DashboardComponent/dashboard.component';
+import { PageNotFoundComponent } from './Pages/PageNotFoundComponent/page-not-found.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'configuration', component: ConfigurationComponent },
+  { path: '',   redirectTo: '/dashboard', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
 ];
 
 @NgModule({
