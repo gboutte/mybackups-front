b86b573a03e2d782e0acdb30dc234b9fc1d99dd3
gboutte <btte.gregory@gmail.com>
2021-09-11 15:26:30 +0200

    Move modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FooterComponent } from './Components/FooterComponent/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './Modules/material.module';
 import { SidebarComponent } from './Components/SidebarComponent/sidebar.component';
 import { MBRootComponent } from './MyBackupsRootComponent/mb-root.component';
 
