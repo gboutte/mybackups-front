eb3f734b3e795d634e48af2a3ddcd358b97b2135
gboutte <btte.gregory@gmail.com>
2021-09-12 19:38:42 +0200

    Add toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { MaterialModule } from './Modules/material.module';
 import { SidebarComponent } from './Components/SidebarComponent/sidebar.component';
 import { MBRootComponent } from './MyBackupsRootComponent/mb-root.component';
 import { ConfigurationModule } from './Modules/configuration.module';
+import { ToastrModule } from 'ngx-toastr';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -21,6 +23,12 @@ import { ConfigurationModule } from './Modules/configuration.module';
     BrowserAnimationsModule,
     MaterialModule,
     ConfigurationModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      countDuplicates:true,
+      preventDuplicates:true
+    }),
+    HttpClientModule,
   ],
   providers: [
     Title,
